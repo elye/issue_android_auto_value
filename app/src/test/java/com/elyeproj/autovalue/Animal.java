735e316bd9ecdd6f893222688f69cb15ca829e4e
elye <elye.project@gmail.com>
2019-10-09 09:21:11 +1100

    Initial commit

---
+++ app/src/test/java/com/elyeproj/autovalue/Animal.java
@@ -0,0 +1,13 @@
+package com.elyeproj.autovalue;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+abstract class Animal {
+    static Animal create(String name, int numberOfLegs) {
+        return new AutoValue_Animal(name, numberOfLegs);
+    }
+
+    abstract String name();
+    abstract int numberOfLegs();
+}
---
+++ app/src/test/java/com/elyeproj/autovalue/ExampleUnitTest.java
@@ -0,0 +1,26 @@
+package com.elyeproj.autovalue;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void testAnimal() {
+        Animal dog = Animal.create("dog", 4);
+        assertEquals("dog", dog.name());
+        assertEquals(4, dog.numberOfLegs());
+
+        // You probably don't need to write assertions like these; just illustrating.
+        assertTrue(Animal.create("dog", 4).equals(dog));
+        assertFalse(Animal.create("cat", 4).equals(dog));
+        assertFalse(Animal.create("dog", 2).equals(dog));
+
+        assertEquals("Animal{name=dog, numberOfLegs=4}", dog.toString());
+    }
+}
